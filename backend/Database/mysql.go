8ebe2aebcb9f96a0e90b8bafd2a1be2d09ded4fc
TaoZhang <zt764851079@gmail.com>
2022-02-02 12:48:29 -0500

    creat database

--- backend/Database/mysql.go
+++ backend/Database/mysql.go
@@ -8,11 +8,49 @@ import (
 	"strings"
 )
 
+//var MysqlDb *sql.DB
+//var MysqlDbErr error
+//
+//const (
+//	USER_NAME = "root"
+//	PASS_WORD = "root123456"
+//	HOST      = "localhost"
+//	PORT      = "3306"
+//	DATABASE  = "IMDB"
+//	CHARSET   = "utf8"
+//)
+
+//// 初始化链接
+//func init() {
+//
+//	dbDSN := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s", USER_NAME, PASS_WORD, HOST, PORT, DATABASE, CHARSET)
+//
+//	// 打开连接失败
+//	MysqlDb, MysqlDbErr = sql.Open("mysql", dbDSN)
+//	//defer MysqlDb.Close();
+//	if MysqlDbErr != nil {
+//		log.Println("dbDSN: " + dbDSN)
+//		panic("数据源配置不正确: " + MysqlDbErr.Error())
+//	}
+//
+//	// 最大连接数
+//	MysqlDb.SetMaxOpenConns(100)
+//	// 闲置连接数
+//	MysqlDb.SetMaxIdleConns(20)
+//	// 最大连接周期
+//	MysqlDb.SetConnMaxLifetime(100 * time.Second)
+//
+//	if MysqlDbErr = MysqlDb.Ping(); nil != MysqlDbErr {
+//		panic("数据库链接失败: " + MysqlDbErr.Error())
+//	}
+//
+//}
+
 //数据库的基础信息
 const (
 	userName = "root"
-	password = "GuoZiHan1998"
-	dbName   = "mydb"
+	password = "root123456"
+	dbName   = "IMDB"
 	host     = "localhost"
 	port     = "3306"
 	charset  = "utf8"
@@ -34,13 +72,9 @@ func GetDataBase() *gorm.DB {
 
 	// 构建连接："用户名:密码@tcp(IP:端口)/数据库?charset=utf8"
 	path := strings.Join([]string{userName, ":", password, "@tcp(", host, ":", port, ")/", dbName, "?charset=", charset}, "")
-	//path := "root:GuoZiHan1998@tcp(localhost:3306)?charset=Utf8&parseTime=true&loc=Local"
 	fmt.Println(path)
-	//// 打开数据库,前者是驱动名，所以要导入： _ "github.com/go-sql-driver/mysql"
+	// 打开数据库,前者是驱动名，所以要导入： _ "github.com/go-sql-driver/mysql"
 	DB, err := gorm.Open("mysql", path)
-
-	//DB, err := gorm.Open("mysql", "root:GuoZiHan1998@tcp(127.0.0.1:3306)/test")
-
 	if err != nil {
 		log.Fatal("连接失败！")
 		return nil
